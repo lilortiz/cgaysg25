b22576d8e39084fff126c5cef071e950616f77f4
lilortiz <167330321+lilortiz@users.noreply.github.com>
2025-07-16 18:23:53 -0300

    Add files via upload

---
+++ cgaysgV2-main/cgaysgV2-main/tp1.js
@@ -0,0 +1,147 @@
+let micTP1, fftTP1;
+let micOn = false;
+let formasTP1 = [];
+let imagenesTP1 = [];
+let ultimaFormaTP1 = null;
+let ultimaFormaH = null;
+
+let alternarDir = true;
+
+//contadores para la gneración de figuras
+let maxVerticales = 15;
+let maxHorizontales = 15;
+let contadorVerticales = 0;
+let contadorHorizontales = 0;
+
+function preload() {
+  for (let i = 1; i <= 5; i++) {
+    imagenesTP1.push(loadImage(`formas/forma${i}.png`));
+  }
+}
+
+function setup() {
+  createCanvas(800, 600);
+  micTP1 = new p5.AudioIn();
+  fftTP1 = new p5.FFT();
+  fftTP1.setInput(micTP1);
+  imageMode(CENTER);
+}
+
+function draw() {
+  background(240, 227, 206, 10);
+  let vol = micTP1.getLevel();
+  console.log("volumen es:" + vol);
+
+  if (vol > 0.009) {
+    let x, y, angulo;
+
+    // se generan formas mientras hay sonido y no se alcanzó el límite
+    if (alternarDir && contadorVerticales < maxVerticales) { 
+      // Verticales
+      if (!ultimaFormaTP1 || ultimaFormaTP1.terminada()) {
+        x = width / 2;
+        y = height * 0.9;
+      } else {
+        x = ultimaFormaTP1.x;
+        y = ultimaFormaTP1.y;
+      }
+
+      let direccion = random([
+        { dx: 0, dy: -40 },
+        { dx: 40, dy: -40 },
+        { dx: -40, dy: -40 }
+      ]);
+
+      ultimaFormaTP1 = generarForma(x, y, direccion, 0); // sin rotación
+      contadorVerticales++; //aumenta el contador para las figuras verticales
+      
+    } else if (!alternarDir && contadorHorizontales < maxHorizontales) {
+      // Horizontales
+      if (!ultimaFormaH || ultimaFormaH.terminada()) {
+        x = width / 2;
+        y = height / 2;
+      } else {
+        x = ultimaFormaH.x;
+        y = ultimaFormaH.y;
+      }
+
+      let direccion = random([
+        { dx: 40, dy: 0 },
+        { dx: -40, dy: 0 }
+      ]);
+
+      // acá se genera un ángulo aleatorio para que rote todo
+      let rotaTodo = random(0, TWO_PI);
+      ultimaFormaH = generarForma(x, y, direccion, rotaTodo); // rotación 180°
+      contadorHorizontales++;//aumenta el contador para las figuras horizontales
+    }
+
+    alternarDir = !alternarDir;
+  }
+
+  // Dibujar y actualizar formas
+  for (let i = formasTP1.length - 1; i >= 0; i--) {
+    formasTP1[i].actualizar();
+    formasTP1[i].mostrar();
+    if (formasTP1[i].terminada()) formasTP1.splice(i, 1);
+  }
+
+  if (formasTP1.length === 0) {
+    ultimaFormaTP1 = null;
+    ultimaFormaH = null;
+    contadorVerticales = 0;
+    contadorHorizontales = 0;
+  }
+}
+
+function generarForma(x, y, direccion, angulo) {
+  let nuevaX = constrain(x + direccion.dx + random(-5, 5), 50, width - 50);
+  let nuevaY = constrain(y + direccion.dy + random(-10, 10), 50, height - 50);
+  let img = random(imagenesTP1);
+  let nueva = new FormaTP1(nuevaX, nuevaY, img, angulo);
+  formasTP1.push(nueva);
+  return nueva;
+}
+
+class FormaTP1 {
+  constructor(x, y, img, angulo) {
+    this.x = x;
+    this.y = y;
+    this.img = img;
+    this.alpha = 255;
+    this.escala = random(0.2, 0.5);
+    this.angulo = angulo || PI * random (0,2);
+  }
+
+  actualizar() {
+    if (!micTP1.enabled || micTP1.getLevel() < 0.05) {
+      this.alpha -= 0.005;
+    }
+  }
+
+  mostrar() {
+    tint(255, this.alpha);
+    push();
+    translate(this.x, this.y);
+    rotate(this.angulo); // Aplicar rotación
+    scale(this.escala);
+    image(this.img, 0, 0);
+    pop();
+  }
+
+  terminada() {
+    return this.alpha <= 0;
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function mousePressed() {
+  userStartAudio();
+  if (!micOn) {
+    micOn = true;
+    micTP1.start();
+  }
+}
